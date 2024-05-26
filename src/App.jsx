e00eaf2501cd75bad376be4609ceba909e7b3e5a
Likhith Kumar <likhithkumar104@gmail.com>
2024-05-26 09:27:23 +0530

    Day5

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from "react";
 import React from 'react'
-import "./App.css";
 
 function App() {
   const [counter, setcounter] = useState(0);
   return (
-     <>
+     <div>
      <h1>The Number {counter}</h1>
      <button onClick={()=>setcounter(counter + 1)}>ADD</button>
      <button onClick={()=>setcounter(counter - 1)}>SUB</button>
-     </>
+     </div>
   );
 }
 
