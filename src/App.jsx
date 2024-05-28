04021bb2807578b530c4a0f9dce9cfe69a89b321
Likhith Kumar <likhithkumar104@gmail.com>
2024-05-28 21:35:35 +0530

    Day 7

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 import useCurrencyInfo from './hooks/useCurrencyInfo'
 import {InputBox} from './components/index.js'
