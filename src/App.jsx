e1047c369c36c21dd889f5b281b6c85b93209bf5
Likhith Kumar <likhithkumar104@gmail.com>
2024-05-28 21:22:48 +0530

    Day6

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,74 @@
-import { useState , useCallback, useEffect, useRef} from "react";
-import React from 'react'
-import './app.css'
-
+import { useState } from 'react'
+import './App.css'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
+import {InputBox} from './components/index.js'
 
 function App() {
+  const [amount, setAmount] = useState(0)
+  const [from, setFrom] = useState('usd')
+  const [to, setTo] = useState('inr')
+  const [convertedAmount, setConvertedAmount] = useState(0)
 
-  const [length ,setlength] = useState(10);
-  const [Numallowed ,setNumallowed] = useState(false);
-  const [Charllowed ,setCharllowed] = useState(false);
-  const [Password ,setpassword] = useState('');
+  const currencyInfo = useCurrencyInfo(from)
+  const options = Object.keys(currencyInfo)
 
-  const passcopy = useRef(null)
+  const swap = () => {
+    setFrom(to)
+    setTo(from)
+    setConvertedAmount(amount)
+    setAmount(convertedAmount)
+  }
 
-  const genaratepassword = useCallback(() => {
-      let pass = "";
-      let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdeghijklmnopqrstuvwxyz";
-      if(Numallowed) str += "0123456789";
-      if(Charllowed) str += "!@#$%^&*()_";
-
-      for(let i=1; i<length; i++){
-        const char = Math.floor(Math.random() * str.length + 1)
-        pass += str.charAt(char)
-      }
-      
-      setpassword(pass)
-  }, [length, Numallowed, Charllowed])
- 
-  useEffect(() => {
-    genaratepassword()
-  }, [length, Numallowed, Charllowed])
-
-  const copypassword = () => {
-    window.navigator.clipboard.writeText(Password);
-    passcopy.current?.select()
+  const convert = () => {
+    setConvertedAmount(amount * currencyInfo[to])
   }
 
   return (
-     <>
-        <div className="container cen">
-          <h1>Passwords Generator</h1>
-          <div>
-            <input type="text" placeholder="Password" value={Password} className="text" readOnly ref={passcopy}/>
-            <button onClick={copypassword}>Cpoy</button>
-          </div>
-          <div  className="cen">
-              <input className="range" type="range" min={6} max={100} value={length} onChange={(e) => setlength(e.target.value) }/>
-              <label htmlFor="length">length : {length}</label>
-          </div>
-          <div  className="cen">
-              <label htmlFor="numbers">Numbers</label>
-              <input className="check" type="checkbox" defaultChecked={Numallowed} onChange={() => {setNumallowed((prev) => !prev)}} />
-          </div>
-          <div  className="cen">
-              <label htmlFor="numbers">Symbols</label>
-              <input className="check" type="checkbox" defaultChecked={Charllowed} onChange={() => {setCharllowed((prev) => !prev)}} />
-          </div>
-
+    <div className='w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat'
+    style={{backgroundImage: `url(https://images.pexels.com/photos/4497591/pexels-photo-4497591.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2)`}}
+    >
+      <div className='w-full'>
+        <div className='w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30'>
+          <form onSubmit={(e) => {
+            e.preventDefault()
+            convert()
+          }}>
+            <div className='w-full mb-1'>
+              <InputBox
+              label="from"
+              amount={amount}
+              currencyOptions={options}
+              onCurrencyChange={(currency) => setFrom(currency)}
+              onAmountChange={(amount) => setAmount(amount)}
+              selectedCurrency={from}
+              />
+            </div>
+            <div className='relative w-full h-0.5'>
+              <button
+              className='absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5'
+              onClick={swap}
+              >Swap</button>
+            </div>
+            <div className='w-full mb-1'>
+              <InputBox
+              label="to"
+              currencyOptions={options}
+              amount={convertedAmount}
+              onCurrencyChange={(currency) => setTo(currency)}
+              selectedCurrency={to}
+              amountDisabled
+              />
+            </div>
+            <button
+            type='submit'
+            className='w-full bg-blue-600 text-white px-4 py-3 rounded-lg'
+            >Convert {from.toUpperCase()} to {to.toUpperCase()}</button>
+          </form>
         </div>
-     </>
-  );
+      </div>
+
+    </div>
+  )
 }
 
-export default App;
+export default App
